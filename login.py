75d19f655fcd347717bc9c86d2af0643c8f778b1
DBraxton6 <dbraxton3@gatech.edu>
2018-07-17 14:23:14 +0200

    add comments

--- login.py
+++ login.py
@@ -1,6 +1,14 @@
 from Tkinter import *
 
+# creates a window
 root = Tk()
+
+# creates a label with text
 theLabel = Label(root, text="This is text")
+
+# "packs" the label somewhere on the screen (no formatting)
 theLabel.pack()
+
+# allows window to be continuously on the screen until exit/minimize
+# is pressed
 root.mainloop()
