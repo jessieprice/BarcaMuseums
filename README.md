9cd0779a241948b17dc6c9dcd444b1282c7b5bbf
DBraxton6 <dbraxton3@gatech.edu>
2018-07-17 14:27:37 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BarcaMuseums
 4400 Final Project
+
+If having tkinter errors:
+Download ActivateTcl from https://www.activestate.com/activetcl/downloads
+
--- login.py
+++ login.py
@@ -9,6 +9,5 @@ theLabel = Label(root, text="This is text")
 # "packs" the label somewhere on the screen (no formatting)
 theLabel.pack()
 
-# allows window to be continuously on the screen until exit/minimize
-# is pressed
+# allows window to be continuously on the screen until exit/minimize is pressed
 root.mainloop()
