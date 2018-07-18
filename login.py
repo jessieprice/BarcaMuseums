433efe51dda41c2bb1de65bdd87e199374ae78d3
DBraxton6 <dbraxton3@gatech.edu>
2018-07-18 12:19:46 +0200

    update login screen

--- login.py
+++ login.py
@@ -3,26 +3,30 @@ from Tkinter import *
 # creates a window
 root = Tk()
 
+label_login = Label(root, text="LOGIN")
+label_login.grid(row=0, columnspan=2)
+
 # login labels
-label_1 = Label(root, text="Username:")
-label_2 = Label(root, text="Password:")
+label_un = Label(root, text="Username:")
+label_pw = Label(root, text="Password:")
 
 # login text fields
-entry_1 = Entry(root)
-entry_2 = Entry(root)
+entry_un = Entry(root)
+entry_pw = Entry(root)
 
 # aligning login label and entry fields
-label_1.grid(row=0, sticky=E)
-label_2.grid(row=1, sticky=E)
-entry_1.grid(row=0, column=1)
-entry_2.grid(row=1, column=1)
+label_un.grid(row=1, sticky=E)
+label_pw.grid(row=2, sticky=E)
+entry_un.grid(row=1, column=1)
+entry_pw.grid(row=2, column=1)
 
 # keep me logged in checkbox
 c = Checkbutton(root, text="Keep me logged in")
 c.grid(columnspan=2)
 
-
-
+# login button
+button1 = Button(root, text="Login")
+button1.grid(row=4, columnspan=2)
 
 # allows window to be continuously on the screen until exit/minimize
 root.mainloop()
